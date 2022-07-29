251521e2c39355b32020cd4c935e53dcbd92ff95
Rohan James <rohanjames9282@gmail.com>
2022-07-29 22:26:21 +0530

    Add display functionality and basic math functions

---
+++ script.js
@@ -0,0 +1,99 @@
+/*--------------------------------------*/
+// DISPLAY/BUTTON FUNCTIONS
+/*--------------------------------------*/
+
+//This array contains all user inputs to display
+
+//Event delegator(so that I don't have to add separate event listener to each button of same type).
+function addGlobalEventListener(type, selector, callback){
+    document.addEventListener(type, e => {
+        if (e.target.matches(selector)) callback(e)
+    })
+}
+
+
+const keyboard = document.querySelector('.keyboard');
+const allBtns  = keyboard.querySelectorAll('button');
+const numbers = document.querySelector('.numbers').querySelectorAll('button');
+const methods = document.querySelector('.methods').querySelectorAll('button');
+const display = document.querySelector('.display');
+const clearBtn = document.querySelector('.clear');
+const equalsBtn = document.querySelector('.equals');
+let input = display.textContent;
+ 
+
+clearBtn.addEventListener('click', () => {
+    display.textContent = ""
+    input = "";
+    calculatedTotal = 0;
+});
+
+
+// Displaying all numbers that user enters to display.
+addGlobalEventListener('click', '.number', e => {
+    display.textContent += e.target.textContent;
+})
+
+//Displaying methods to display only if the last character in the display is not a method and there is atleast one number on the display. Here we use displayContent.length - 2 because method character has space in front of it.
+addGlobalEventListener('click', '.method', e => {
+    let displayContent = display.textContent;
+    if(/[-+/x%]/.test(displayContent[displayContent.length - 2]) == false && /[0-9]+/g.test(displayContent) == true){
+    display.textContent += ` ${e.target.textContent} `}
+});
+
+//Getting user input to run methods
+equalsBtn.addEventListener('click', () => {
+    input = display.textContent.split(" ")
+    calc(input);
+})
+
+/*--------------------------------------*/
+// CALCULATION FUNCTIONS
+/*--------------------------------------*/
+//Our final number.
+let calculatedTotal = 0;
+
+function calc (input){
+    
+    for(let j = 0; j < input.length; j++){
+        if(/[0-9]+/g.test(input[j]) == true){
+            input[j] =  parseInt(input[j]);
+        }
+    }
+    console.log(input)
+
+    for(let i = 0; i < input.length; i++){
+        switch(true){
+            case input[i] == '+':       add(input[i - 1], input [i + 1]); break;
+            case input[i] == '-':       sub(input[i - 1], input [i + 1]); break;
+            case input[i] == '/':       divide(input[i - 1], input [i + 1]); break;
+            case input[i] == 'x':       multiply(input[i - 1], input [i + 1]); break;
+            case input[i] == '%':       percentage(input[i - 1], input [i + 1]); break;
+            // case input[i] == 'sq':   square(input[i - 1], input [i + 1]);
+        }
+    }
+    display.textContent = calculatedTotal;
+}
+
+function add(a, b){
+    console.log(typeof(a))
+    return calculatedTotal += a + b;
+}
+
+function sub(a, b){
+    return  calculatedTotal += a - b;
+}
+
+function divide(a, b){
+    return calculatedTotal += a / b;
+}
+
+function multiply(a, b) {
+    return calculatedTotal += a * b;
+}
+
+function percentage(a, b){
+    return calculatedTotal += (b / a) * 100;
+}
+
+  
